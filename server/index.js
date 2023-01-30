7f52a7658f644dd6c20b92984245b9aac1227892
Giovanni Medrano <giovannimedrano@Giovannis-MBP.hitronhub.home>
2023-01-30 09:51:07 -0600

    fixes

---
+++ server/index.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+
+import postRoutes from './routes/postRoutes.js';
+import dalleRoutes from './routes/dalleRoutes.js';
+import connectDB from './mongodb/connect.js';
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json({limit: '50mb'}));
+app.use('/api/v1/post', postRoutes);
+app.use('/api/v1/dalle', dalleRoutes);
+
+app.get('/', async (req, res) => {
+    res.send('Hello from DALL-Eeeee')
+});
+
+
+const startServer = async () =>{
+    try {
+        connectDB(process.env.MONGODB_URL);
+        app.listen(8080, () => console.log('Sever has started on port 8080'))
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+startServer();
